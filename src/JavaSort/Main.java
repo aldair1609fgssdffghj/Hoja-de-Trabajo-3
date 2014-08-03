9bba1c8da74945bd803cf0d626c1ffd335119465
gar13177 <gar13177@uvg.edu.gt>
2014-08-03 15:32:16 -0600

    Main

--- src/JavaSort/Main.java
+++ src/JavaSort/Main.java
@@ -6,5 +6,11 @@ package JavaSort;
  * @author Kevin
  */
 public class Main {
+    public static void main (String[] args){
+        
+        RandomArray newRandom = new RandomArray(10);
+        
+        int[] _array = newRandom.getArray();
+    }
     
 }
