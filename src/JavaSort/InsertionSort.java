b172398c4f153b7996c44d81b0082d498a4fabe2
gar13177 <gar13177@uvg.edu.gt>
2014-08-03 15:58:43 -0600

    InsertionSort completed

--- src/JavaSort/InsertionSort.java
+++ src/JavaSort/InsertionSort.java
@@ -17,8 +17,8 @@ public class InsertionSort extends Sort {
             int j;
             for(j =i-1;j>=0 && temp < a[j]; j--)
                 a[j+1]=a[j];
-            a[j+1]=temp;
-           
+            a[j+1]=temp;  
         }
+        _sortedArray = a;
     }
 }
